76736262729933c2185c91a816d002a4677a141d
Mathias <mip@ruc.dk>
2020-11-28 12:46:30 +0100

    knockout added

--- DataServiceLib/Databasecontext.cs
+++ DataServiceLib/Databasecontext.cs
@@ -20,7 +20,7 @@ namespace DataServiceLib
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLoggerFactory(MyLoggerFactory);
-            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=Jegersej19");
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=11191Ippe");
         }
 
         internal static object FromSql(string v)
