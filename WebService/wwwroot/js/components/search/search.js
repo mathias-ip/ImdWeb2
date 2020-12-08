6b60bf998d77ce335d602eda4e30a42bd1ca8d53
Mathias <mip@ruc.dk>
2020-12-08 11:55:44 +0100

    Kan søge på Navn

--- WebService/wwwroot/js/components/search/search.js
+++ WebService/wwwroot/js/components/search/search.js
@@ -1,28 +1,34 @@
-﻿define(['postman'], (postman) => {
+﻿define(['postman','knockout'], (postman, ko) => {
     return function () {
-
+        
+        let names = ko.observableArray();
         let gotoContact = () => {
             postman.publish("changeContent", "search");
         }
 
+        let id = ko.observable();
+        let getNames = () => {
+            console.log('api/Titles/search/' + id());
+            fetch('api/Titles/search/' + id())
+                .then(response => response.json())
+                .then(names);
+
+        };
+
+        //getNames();
+
+        
+
         return {
-            gotoSearch
+            gotoContact,
+            names,
+            id,
+            getNames
         };
     }
 });
 
-define(['knockout']), (ko) => {
-    let names = ko.observableArray();
 
-}
 
-let getNames = (callback) => {
-    fetch('api/Titles/search/'+ id)
-        .then(response => response.json())
-        .then(callback);
 
-};
 
-getNames(x => {
-    names(x)
-});
