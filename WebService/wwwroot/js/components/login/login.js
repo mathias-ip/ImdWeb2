45548406ce8c6657f8b8668df4bc1d4cd6ecbb39
Mathias <mip@ruc.dk>
2021-01-24 15:10:52 +0100

    it works!!

--- WebService/wwwroot/js/components/login/login.js
+++ WebService/wwwroot/js/components/login/login.js
@@ -1,26 +1,13 @@
-﻿define(['postman', 'knockout', 'viewModel'], (postman, ko, vm) => {
+﻿define(['postman', 'knockout'], (postman, ko) => {
     return function (params) {
-        
+
+
         //laver variable navne til login
         let username = ko.observable();
         let password = ko.observable();
         let email = ko.observable();
-        let checkUsername = () => {
-           
-            ds.verifyUser(username(), password(), user => {
-                if (user !== undefined) {
-                    vm.user(user);
-                    alert('You have succesfully logged in!');
-                    vm.changeContent("Home");
-                   
-                }
-                else {
-                    alert('Username or password is incorrect.');
-                    username('');
-                    password('');
-                }
-            });
-            //mathias...
+
+
         //krav til user i form af brugernavn, password og email
         let createUser = () => {
             let user = {
@@ -29,27 +16,23 @@
                 email: email()
             };
 
-            //Sender dataen tilbage til datalaget ved hjælp af post
-            fetch('user/CreateUser/', {
-                method: 'POST',
-                body: JSON.stringify(user),
+
+                //Sender dataen tilbage til datalaget ved hjælp af post
+                fetch('user/CreateUser/', {
+                    method: 'POST',
+                    body: JSON.stringify(user),
                 headers: {
-                    
+
                     'Content-Type': 'application/json;charset=UTF-8'
                 }
-
             });
 
-
- 
         };
-
         return {
             username,
             password,
             email,
             createUser
-            checkUsername
         };
     }
 });
